012baa681dd27ed4fb363712695e9561c7eef100
Daniel Acevedo <daniacevedo205@gmail.com>
2025-11-02 09:50:58 -0300

    first commit

---
+++ src/main/java/com/example/Cobre_challenge/CobreChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.Cobre_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CobreChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CobreChallengeApplication.class, args);
+	}
+
+}
